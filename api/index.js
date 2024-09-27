167cb2a1f4a77c868a6adcd0a5794976278327c3
Rajesh <nol151740@gmail.com>
2024-09-27 16:33:04 +0545

    create user update api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -17,8 +18,10 @@ mongoose
 
 const app = express();
 
+
 // Middleware to parse JSON
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/user', userRouter);  // '/api/user' route
 app.use('/api/auth', authRouter);  // '/api/auth' route
