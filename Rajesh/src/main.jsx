5efa76adca8d2cf2a04e057f2ab35f50f0c297d6
Rajesh <nol151740@gmail.com>
2024-09-27 09:19:22 +0545

    add redux toolkit

--- Rajesh/src/main.jsx
+++ Rajesh/src/main.jsx
@@ -1,14 +1,12 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { store } from './redux/store';  // Removed the .js extension, it's optional and often omitted
-import { Provider } from 'react-redux';
+import {  store } from './redux/store.js';
+import { Provider } from "react-redux";
 
-createRoot(document.getElementById('root')).render(
-    <StrictMode>  {/* Wrap the app in StrictMode for highlighting potential issues */}
-        <Provider store={store}>
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Provider store={store}>
             <App />
-        </Provider>
-    </StrictMode>
+    </Provider>
 );
--- Rajesh/src/redux/store.js
+++ Rajesh/src/redux/store.js
@@ -1,11 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './user/userSlice';  // Fixed the typo
+import { configureStore } from '@reduxjs/toolkit'
+import userReducer from './user/userSlice.js'
 
 export const store = configureStore({
-    reducer: {
-        user: userReducer,
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-        serializableCheck: false,  // Fixed middleware option to 'serializableCheck'
+    reducer: {user: userReducer},
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+        serializable: false,
     }),
 });
